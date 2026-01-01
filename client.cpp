c9a01c44384b9e24e8452ed189d8933be1bcfe56
antho <antho@example.com>
2026-01-01 17:21:44 -0500

    add windows compatibility

--- client.cpp
+++ client.cpp
@@ -1,14 +1,60 @@
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netinet/ip.h>  
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#ifdef _WIN32
+    #include <winsock2.h>
+    #include <ws2tcpip.h>
+    #define read(fd, buf, n) recv(fd, buf, n, 0)
+    #define write(fd, buf, n) send(fd, buf, n, 0)
+    #define close closesocket
+    typedef int socklen_t;
+#else
+    #include <unistd.h>
+    #include <arpa/inet.h>
+    #include <sys/socket.h>
+    #include <netinet/ip.h>
+#endif
 
 
-int main(){
+static void die(const char *msg) {
+    int err = errno;
+    fprintf(stderr, "[%d] %s\n", err, msg);
+    abort();
+}
+
+int main() {
+#ifdef _WIN32
+    WSADATA wsa;
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+        die("WSAStartup()");
+    }
+#endif
 
     int fd = socket(AF_INET, SOCK_STREAM, 0);
     if (fd < 0) {
-        die("socket()")
+        die("socket()");
     }
 
+    struct sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = ntohs(1234);
+    addr.sin_addr.s_addr = ntohl(INADDR_LOOPBACK);  // 127.0.0.1
+    int rv = connect(fd, (const struct sockaddr *)&addr, sizeof(addr));
+    if (rv) {
+        die("connect");
+    }
+
+    char msg[] = "hello";
+    write(fd, msg, strlen(msg));
+
+    char rbuf[64] = {};
+    ssize_t n = read(fd, rbuf, sizeof(rbuf) - 1);
+    if (n < 0) {
+        die("read");
+    }
+    printf("server says: %s\n", rbuf);
+    close(fd);
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,50 +1,90 @@
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netinet/ip.h>  
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#ifdef _WIN32
+    #include <winsock2.h>
+    #include <ws2tcpip.h>
+    #define read(fd, buf, n) recv(fd, buf, n, 0)
+    #define write(fd, buf, n) send(fd, buf, n, 0)
+    #define close closesocket
+    typedef int socklen_t;
+#else
+    #include <unistd.h>
+    #include <arpa/inet.h>
+    #include <sys/socket.h>
+    #include <netinet/ip.h>
+#endif
 
 
+static void msg(const char *msg) {
+    fprintf(stderr, "%s\n", msg);
+}
+
+static void die(const char *msg) {
+    int err = errno;
+    fprintf(stderr, "[%d] %s\n", err, msg);
+    abort();
+}
 
 static void do_something(int connfd) {
     char rbuf[64] = {};
-    ssize_t n = read(connfd, rbuf, sizeof(rbuf) -1);
+    ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1);
     if (n < 0) {
         msg("read() error");
         return;
     }
-    printf("client says: %s\n", rbuf);
+    fprintf(stderr, "client says: %s\n", rbuf);
 
     char wbuf[] = "world";
     write(connfd, wbuf, strlen(wbuf));
-    
 }
 
 int main() {
+#ifdef _WIN32
+    WSADATA wsa;
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+        die("WSAStartup()");
+    }
+#endif
+
     int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd < 0) {
+        die("socket()");
+    }
+
+    // this is needed for most server applications
     int val = 1;
-    setsockopt(fd, SOL_SOCKET, SO_REUSDEADDR, &val, sizeof(val));
+    setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, (const char *)&val, sizeof(val));
+
+    // bind
     struct sockaddr_in addr = {};
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(1234);
-    addr.sin_addr.s_addr = htonl(0);
-    int rv = bind(fd, (const struct sockaddr_in *)&addr, sizeof(addr));
-    if (rv) { die("bind()"); }
-    int bind(int sockfd, const struct sockaddr_in *addr, socklen_t addrlen);
+    addr.sin_port = ntohs(1234);
+    addr.sin_addr.s_addr = ntohl(0);    // wildcard address 0.0.0.0
+    int rv = bind(fd, (const struct sockaddr *)&addr, sizeof(addr));
+    if (rv) {
+        die("bind()");
+    }
 
+    // listen
     rv = listen(fd, SOMAXCONN);
-    if (rv) {die("listen()");}
+    if (rv) {
+        die("listen()");
+    }
 
-    while (true){
-        //accept
+    while (true) {
+        // accept
         struct sockaddr_in client_addr = {};
-        socklen_t addrlen = sizeof(client_addt);
-        int connfd = accept(fd, (struct sockaddr_in *)&client_addr, &addrlen);
+        socklen_t addrlen = sizeof(client_addr);
+        int connfd = accept(fd, (struct sockaddr *)&client_addr, &addrlen);
         if (connfd < 0) {
-            continue;
+            continue;   // error
         }
 
         do_something(connfd);
         close(connfd);
-
     }
 
     return 0;
