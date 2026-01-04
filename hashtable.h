678b73974bf51c2b3a800539f795f64acef59ed3
antho <antho@example.com>
2026-01-04 14:51:31 -0500

    more hash functions

--- hashtable.h
+++ hashtable.h
@@ -12,3 +12,12 @@ struct HTab {
     size_t size = 0;
 };
 
+struct HMap{
+    HTab newer;
+    HTab older;
+    size_t migrate_pos = 0;
+};
+
+HNode *hm_lookup(HMap *hmap, HNode *key, bool (*eq)(HNode *, HNode *));
+void hm_insert(HMap *hmap, HNode *node);
+HNode *hm_delete(HMap *hmap, HNode *key, bool (*eq)(HNode *, HNode *));
